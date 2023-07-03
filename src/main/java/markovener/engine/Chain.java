9c69b67056e74f9a9d736742853f95d7a96ca99c
Arthur-Brenno <64020210+arthurbrenno@users.noreply.github.com>
2023-07-03 16:26:50 -0300

    added Json conversion

--- src/main/java/markovener/engine/Chain.java
+++ src/main/java/markovener/engine/Chain.java
@@ -1,9 +1,12 @@
-package newengine;
+package markovener.engine;
+import com.google.gson.Gson;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class Chain {
 
@@ -29,6 +32,18 @@ public final class Chain {
       return new Chain(filteredText);
    }
 
+   public String toJson() {
+      return new Gson().toJson(map);
+   }
+
+   public List<String> tokens() {
+      return tokens;
+   }
+
+   public Map<String, List<String>> map() {
+      return map;
+   }
+
    private @NotNull List<String> tokenizeNgrams(@NotNull String text, int order) {
       List<String> result = new ArrayList<>();
       for (int i = 0; i <= text.length() - order; ++i) {
@@ -37,11 +52,11 @@ public final class Chain {
       return result;
    }
 
-   private @NotNull List<String> tokenizeWords(@NotNull String text) {
+   private @NotNull @Unmodifiable List<String> tokenizeWords(@NotNull String text) {
       return List.of(text.split(" "));
    }
 
-   private @NotNull HashMap<String, List<String>> mapNgrams(@NotNull List<String> tokens) {
+   private @NotNull HashMap<String, List<String>> mapNgrams(@NotNull final List<String> tokens) {
       final HashMap<String, List<String>> chain = new HashMap<>(tokens.size());
       final int order = tokens.get(0).length();
       for (int i = 0; i < tokens.size() - order; ++i) {
@@ -56,7 +71,7 @@ public final class Chain {
       return chain;
    }
 
-   private @NotNull HashMap<String, List<String>> mapWords(@NotNull List<String> tokens) {
+   private @NotNull HashMap<String, List<String>> mapWords(@NotNull final List<String> tokens) {
       final HashMap<String, List<String>> chain = new HashMap<>(tokens.size());
       for (int i = 0; i < tokens.size() - 1; ++i) {
          String key = tokens.get(i);
@@ -69,5 +84,4 @@ public final class Chain {
       }
       return chain;
    }
-
 }
